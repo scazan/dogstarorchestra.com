aeab3968ff2fc110d8e87537261fdb9f2bcdc8fe
scazan <scottcazan@gmail.com>
2019-07-15 11:38:40 +0200

    Adding in config with env variables

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export default {
+  SPACE_ID: process.env.REACT_APP_SPACE_ID ? process.env.REACT_APP_SPACE_ID : '',
+  ACCESS_TOKEN: process.env.REACT_APP_ACCESS_TOKEN ? process.env.REACT_APP_ACCESS_TOKEN : '',
+}
